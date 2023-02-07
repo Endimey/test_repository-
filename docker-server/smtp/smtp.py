56af7188da91c6535732d50d65d3cbd83dd3c6d8
Endimey <61121043+Endimey@users.noreply.github.com>
2023-02-07 12:28:28 +0600

    Update smtp.py

--- docker-server/smtp/smtp.py
+++ docker-server/smtp/smtp.py
@@ -4,7 +4,7 @@ import smtplib
 smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
 smtpObj.starttls()
 
-smtpObj.login('yurov.mikhail@gmail.com', 'PlayLife55')
+smtpObj.login('yurov.mikhail@gmail.com', 'password')
 
 smtpObj.sendmail("yurov.mikhail@gmail.com" "endimey@gmail.com", "Пора баиньки!")
 
